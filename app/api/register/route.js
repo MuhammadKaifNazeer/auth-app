61a8270ee39492f17e11b7d4e6fc4c9d7b2a7c62
Muhammad Kaif Nazeer <muhammadkaifnazeer99@gmail.com>
2024-04-08 13:13:12 +0500

    added mongodb string

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,19 +1,18 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-
   try {
     const { name, email, password } = await req.json();
 
-    console.log("Name : ", name);
-    console.log("Email : ", email);
-    console.log("Password : ", password);
+    console.log("Name: ", name);
+    console.log("Email: ", email);
+    console.log("Password: ", password);
 
-    return NextResponse.json({ message: "User registered" }, { status: 201 });
+    return NextResponse.json({ message: "User registered." }, { status: 201 });
   } catch (error) {
     return NextResponse.json(
-        { message: "An error occured while registering the user" },
-        {status: 500}
-        );
+      { message: "An error occurred while registering the user." },
+      { status: 500 }
+    );
   }
 }
