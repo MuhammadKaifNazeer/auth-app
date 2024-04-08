c6aa21aa6ba0a2d5a80cf3275dc241e67a4b24e8
Muhammad Kaif Nazeer <muhammadkaifnazeer99@gmail.com>
2024-04-08 13:37:18 +0500

    created user schema and model

---
+++ models/user.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema, medels } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = medels.User || mongoose.model("User", userSchema);
+export default User;
